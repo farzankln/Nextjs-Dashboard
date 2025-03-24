595ea493608a8851a7be474e2f7ed4359a7a8e07
farzan8304 <farzankalantari.pg@gmail.com>
2025-03-24 22:22:39 +0330

    Error Handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -11,7 +12,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   ]);
 
   if (!invoice) {
-    return <p>Invoice not found</p>;
+    notFound();
   }
 
   return (
